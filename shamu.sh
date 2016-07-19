aa497346c8eed6fedce4674483c2a07d5459bff7
TeamFahQ <teamfahq1@gmail.com>
2016-07-18 23:26:23 -0500

    better way of exiting commands

--- shamu.sh
+++ shamu.sh
@@ -6,9 +6,9 @@ root=/media/thomas/Dev/BrokenOs
 
 while getopts "clean:dirty:official:kernel:" opt; do
     case "$opt" in
-    c) $root/build-broken.sh -c1 -p -a -j10 shamu && exit;;
-    d) $root/build-broken.sh -c2 -p -a -j10 shamu && exit;;
-    o) $root/build-broken.sh -c10 -p -a -j10 shamu && exit ;;
+    c) $root/build-broken.sh -c1 -p -a -j10 shamu; exit;;
+    d) $root/build-broken.sh -c2 -p -a -j10 shamu; exit;;
+    o) $root/build-broken.sh -c10 -p -a -j10 shamu; exit ;;
     k) source $root/build/envsetup.sh && lunch broken_shamu-userdebug && make bootimage -j12 ;;
     esac
 done
