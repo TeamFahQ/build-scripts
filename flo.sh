993c0bb60eec60a318484ffbb6bdf4e5d414c742
TeamFahQ <teamfahq1@gmail.com>
2017-04-03 00:16:10 -0500

    Tweaking and cleaning up the scipts

--- flo.sh
+++ flo.sh
@@ -6,14 +6,17 @@ root=$PWD
 init= source build/envsetup.sh; breakfast aquarios_flo-userdebug
 rom='make bacon -j10'
 kernel='make bootimage -j18'
+clean='make clean -j36'
+clr='reset'
+prnt='echo -e \033[1mOut with the trash\033[0m'
 
 while getopts "clean:dirty:official:kernel:sync" opt; do
     case "$opt" in
-    c) date;$init && make clean && reset && $rom; exit;;
-    d) date;$init && $rom; exit;;
-    o) date;echo wip; exit ;;
-    k) date;$root; $init; $kernel;exit ;;
-    s) date;repo sync -j4 --force-sync && reset;exit ;;
+    c) $init; $clean; $clr; $prnt; $rom;exit;;
+    d) $init; $rom; exit;;
+    o) echo wip; exit ;;
+    k) $root; $init; $kernel;exit ;;
+    s) repo sync -j4 --force-sync; $clr;exit ;;
     esac
 done
 
